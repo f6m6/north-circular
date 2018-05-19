a90aa5e2c7cb4c2e486abe7173f22e396a795f9b
Farhan Mannan <farhanmannan@mac.com>
2018-05-19 17:09:32 +0100

    Add image and update easter egg

--- script.js
+++ script.js
@@ -2,6 +2,14 @@ function space(s) {
   return s.split('').join(' ');
 }
 
+function titleStyle(s) {
+  return "[" + s.toLocaleUpperCase() + "]";
+}
+
+function restyleHeader(s) {
+ return space(titleStyle(s));
+}
+
 function setHeader() {
-  document.getElementById("h1").innerHTML = space("North Circular");
+  document.getElementById("h1").innerHTML = restyleHeader("North Circular");
 }
